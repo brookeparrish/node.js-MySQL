cf8891c3c53acc26f749ccad3e0a94793ebccec3
Brooke <parrish.e.brooke@gmail.com>
2019-11-04 20:20:19 -0800

    added some functionality

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,13 +11,48 @@ const connection = mysql.createConnection({
 
 connection.connect(function (error) {
     if (error) throw error;
-    connection.query("SELECT `item_id`, `product_name`, `price` FROM products", function (error, result) {
+
+    connection.query("SELECT item_id, product_name, price FROM products", function (error, results) {
         if (error) throw error;
-        for (i = 0; i < result.length; i++) {
-            console.log(`Item ID: ${result[i].item_id}`);
-            console.log(`Product Name: ${result[i].product_name}`);
-            console.log(`Price: ${result[i].price}`);
+        for (i = 0; i < results.length; i++) {
+            console.log(`Item ID: ${results[i].item_id}`);
+            console.log(`Product Name: ${results[i].product_name}`);
+            console.log(`Price: ${results[i].price}`);
             console.log(`----------------------`);
         }
+        productToBuy();
     });
 });
+
+function productToBuy() {
+    inquirer
+        .prompt([
+            {
+                name: "productID",
+                type: "number",
+                message: "What is the Item ID of the product you would like to purchase?",
+            },
+            {
+                name: "quantity",
+                type: "number",
+                message: "How many units of the product would you like to buy?"
+            }
+        ])
+        .then(function (answer) {
+            connection.query(`SELECT stock_quantity FROM products WHERE item_id = ${answer.productID}`, function (error, results) {
+                if (error) throw error;
+                if (results === undefined || results.length == 0) {
+                    console.log("Please select a valid ID");
+                }
+                else if (answer.quantity > results[0].stock_quantity) {
+                    console.log("I'm sorry, there are not that many items available.");
+                } else {
+                    console.log("Congratulations!");
+                }
+            });
+            connection.end(function(err) {
+              });
+        });
+
+}
+
